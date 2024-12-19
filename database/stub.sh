79cb201233bb3dea44d995c13864c2488b2e364a
GachiLord <name504172@gmail.com>
2024-12-19 16:52:48 +0500

    add fake guests

--- database/stub.sh
+++ database/stub.sh
@@ -10,3 +10,6 @@ docker exec hotel2000 psql -U postgres -d hotel2000 -c "COPY goods(title, price)
 
 docker cp ./orders.csv hotel2000:/orders.csv
 docker exec hotel2000 psql -U postgres -d hotel2000 -c "COPY orders(has_paid, item_id, sold_for, amount, created_at) FROM '/orders.csv' DELIMITER ',' CSV HEADER;"
+
+docker cp ./guests.csv hotel2000:/guests.csv
+docker exec hotel2000 psql -U postgres -d hotel2000 -c "COPY guests(name,passport,phone) FROM '/guests.csv' DELIMITER ',' CSV HEADER;"
